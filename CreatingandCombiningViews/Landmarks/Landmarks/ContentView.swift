7226f8e135c1c636cd96b08cdf7a44e7851dc648
Jarren Campos <jarrenmcampos@gmail.com>
2020-05-08 18:18:16 -0700

    initial setup

---
+++ CreatingandCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jarren Campos on 5/8/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
